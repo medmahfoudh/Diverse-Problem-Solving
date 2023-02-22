cb24767272b61357f179e6ee6e95b3e1f23b292e
medmahfoudh <medmahfoudhbouna@gmail.com>
2023-02-22 14:48:40 +0100

    Finding the Largest and Smallest Number in a List

---
+++ largest_smallest.py
@@ -0,0 +1,16 @@
+def find_largest_and_smallest(numbers):
+    if not numbers:
+        return "The list is empty."
+    else:
+        smallest = largest = numbers[0] #3 , 8 , 9 , 190 , 283 , 1 , -2
+        for number in numbers:
+            if number < smallest:
+                smallest = number
+            if number > largest:
+                largest = number
+        return f"The largest number is {largest} and the smallest number is {smallest}."
+
+
+
+numbers = [3 , 8 , 9 , 190 , 283 , 1 , -2]
+print(find_largest_and_smallest(numbers))
