9fc8a6accd51b16a0ed13e573c9847139e116859
medmahfoudh <medmahfoudhbouna@gmail.com>
2023-02-27 19:55:01 +0100

    Maximum Subarray Sum Code

---
+++ max_subarray_sum.py
@@ -0,0 +1,10 @@
+def max_subarray_sum(nums):
+    n = len(nums)
+    memo = [0] * n
+    memo[0] = nums[0]
+    max_sum = nums[0]
+
+    for i in range(1, n):
+        memo[i] = max(nums[i], nums[i] + memo[i-1])
+        max_sum = max(max_sum, memo[i])
+    return max_sum
