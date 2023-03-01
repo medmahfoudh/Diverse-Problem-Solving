8d7adcd0ab7020c6a32c4d0897e8dc06546e8d17
medmahfoudh <medmahfoudhbouna@gmail.com>
2023-03-01 21:49:21 +0100

    Problem Find Duplicates code

---
+++ find_duplicates.py
@@ -0,0 +1,9 @@
+def find_duplicates(nums):
+    seen = set()
+    duplicates = set()
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
